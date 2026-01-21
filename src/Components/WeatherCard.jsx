a0243c5396d7f429560732f39b89b6ce37ff5ab6
basit-GOK <basit.ahmed@geeksofkolachi.com>
2026-01-21 13:53:07 +0500

    Update WeatherCard.jsx

--- src/Components/WeatherCard.jsx
+++ src/Components/WeatherCard.jsx
@@ -1,38 +1,37 @@
 import {useEffect, useState} from "react";
 
 const WeatherCard = ({ tempInfo }) => {
-const [weatherState,setWeatherState] = useState("");
+  const [weatherState, setWeatherState] = useState("");
   const { temp, humidity, pressure, country, sunset, speed, name, weathermood } = tempInfo;
-
-
+  
   // converting the seconds into time
-let sec = sunset;
-let date = new Date(sec * 1000);
-let timeStr = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
+  let sec = sunset;
+  let date = new Date(sec * 1000);
+  let timeStr = date.toLocaleString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true });
 
-useEffect(() => {
- if (weathermood) {
-  switch (weathermood) {
-    case "Clouds":
-      setWeatherState("wi-day-cloudy")
-      break;
-      case "Haze":
-      setWeatherState("wi-fog")
-      break;case "Clear":
-      setWeatherState("wi-day-sunny")
-      break;
-    default:
-      setWeatherState("wi-day-sunny")
-      break;
-  }
- }
-}, [weathermood])
-
+  useEffect(() => {
+    if (weathermood) {
+      switch (weathermood) {
+        case "Clouds":
+          setWeatherState("wi-day-cloudy");
+          break;
+        case "Haze":
+          setWeatherState("wi-fog");
+          break;
+        case "Clear":
+          setWeatherState("wi-day-sunny");
+          break;
+        default:
+          setWeatherState("wi-day-sunny");
+          break;
+      }
+    }
+  }, [weathermood]);
 
   return (
     <article className="widget">
       <div className="weatherIcon">
-        <i className={`wi ${weatherState} `}></i>
+        <i className={`wi ${weatherState}`}></i>
       </div>
       <div className="weatherInfo">
         <div className="temperature">
